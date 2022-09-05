497c9eeff52de3955f77ee05486df7e9159beac5
Gustavo <go609127@gmail.com>
2022-09-05 17:48:36 -0300

    Create firebase.jsx

---
+++ front/src/firebase.jsx
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyCMTQPm276kqXyx33wwC4MGUheBrOiEwbo",
+  authDomain: "plenary-osprey-355222.firebaseapp.com",
+  projectId: "plenary-osprey-355222",
+});
+
+export default firebaseApp;
